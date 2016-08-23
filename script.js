aa4d82ed12285739e6a2a88d65ddec2bcd2d26f1
Chan Jun Shern <chanjunshern@gmail.com>
2016-08-23 14:54:05 +0800

    Slide generation and font size fixed!

--- script.js
+++ script.js
@@ -2,10 +2,14 @@
 function createSlides() {
 	var editorContent = editor.getValue();
 	var contentLines = editorContent.split("\n");
+
+	$('.slides').empty(); // Delete all slides to start afresh
 	for (var i=0; i<contentLines.length; i++) {
 		var newSlide = document.createElement("SECTION");
 		var t = document.createTextNode(contentLines[i]);
-		newSlide.appendChild(t);
+		newSlide.appendChild(t); 
+		newSlide.setAttribute("data-background-color", "#fff");
 		$('.slides').append(newSlide);
 	}
+	Reveal.slide(0,0,0); // Return to first slide
 }
