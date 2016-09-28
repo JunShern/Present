334e63b808ccf2391dba4e285a29912a4018e90b
junshern <chanjunshern@gmail.com>
2016-09-28 22:46:34 +0100

    Line-by-line topic search working, dumped below editor

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 
-function search(query) {
+function search(query, slideId) {
 	$("#content").empty(); 
+	//var imageURL = "http://www.badmintoncafe.com/wp-content/uploads/2014/11/lee-chong-wei.jpg";// response.items[0].link;
+	//$('#'+slideId).attr("data-background-image", imageURL);
 	$.get("https://www.googleapis.com/customsearch/v1?cx=006285665057136291577%3A8i6a7ag49hs&filter=0&searchType=image&\
 		key=AIzaSyDJG_126BKdh0TquHZ1MmRr6spD5_JPubc&q="+query, function(response) {
-		for (var i = 0; i < response.items.length; i++) {
-			var item = response.items[i];
-			// in production code, item.htmlTitle should have the HTML entities escaped.
-			document.getElementById("content").innerHTML += "<img src='" + item.link + "' height=80px>";
-		}
+		var imageURL = "http://www.badmintoncafe.com/wp-content/uploads/2014/11/lee-chong-wei.jpg";// response.items[0].link;
+		$('#'+slideId).attr("data-background-image", imageURL);
+
+		document.getElementById("content").innerHTML += "<p>" + query + "</p>";
+		document.getElementById("content").innerHTML += "<img src='" + response.items[0].link + "' height=60px>";
+		// for (var i = 0; i < response.items.length; i++) {
+		//  	var item = response.items[i];
+		//  	// in production code, item.htmlTitle should have the HTML entities escaped.
+		//  	document.getElementById("content").innerHTML += "<img src='" + item.link + "' height=80px>";
+		// }
 	});
 }
 
@@ -17,16 +24,24 @@ function createSlides() {
 
 	$('.slides').empty(); // Delete all slides to start afresh
 	for (var i=0; i<contentLines.length; i++) {
+		var slideId = 'slide' + i;
+		// Create slide
 		var newSlide = document.createElement("SECTION");
 		var t = document.createTextNode(contentLines[i]);
-		newSlide.appendChild(t); 
+		newSlide.appendChild(t);
 		newSlide.setAttribute("data-background-color", "#fff");
+		// Search for appropriate background image
+		newSlide.setAttribute('id', slideId);
+		var imageURL = "http://www.badmintoncafe.com/wp-content/uploads/2014/11/lee-chong-wei.jpg";// response.items[0].link;
+		//$('#'+slideId).attr("data-background-image", imageURL);
+		//newSlide.setAttribute("data-background-image", imageURL);
+		var topics = nlpAnalysis(contentLines[i]);
+		if (topics.length) {
+			search(topics[0]);
+		}
 		$('.slides').append(newSlide);
 	}
 	Reveal.slide(0,0,0); // Return to first slide
-	var topics = nlpAnalysis(editorContent);
-
-	search(topics[0]);
 }
 
 function nlpAnalysis(strInput) {
